c9471af2f4729b0acbb158141940485dfe304225
David Santana <dsantana@email.arizona.edu>
2016-10-07 15:42:30 -0700

    my list changed

--- src/MyList.java
+++ src/MyList.java
@@ -5,32 +5,30 @@
  * At the very least start each method with your particular structure.
  * This is an example to demonstrate what happens when you and a partner make changes
  * to the same file, it is not intended to evaluate your ability to write a data structure.
- * This file is changed
  */
 public class MyList {
 
-	private class Node {
-		private String data;
-		private Node next;
-	}
-
-	private Node head;
+	String[] array;
 
 	public MyList() {
-		this.head = null;
+		this.array = new String[0];
 	}
-
+	
 	/**
 	 * Adds the element as the first element of the list.
 	 * You don't need to implement this fully, just have something that is a start
 	 * using the appropriate structure.
 	 */
 	public void addFirst(String element) {
-		// Make a new node, with the element, then adjust head.
-		Node newHead = new Node();
-		newHead.data = element;
-		newHead.next = this.head;
-		this.head = newHead;
+		// Very bad implementation, make a new array with one more element than
+		// the current array, add the element at index 0 and then copy over the
+		// previous elements. The adjust this.array to point at the new array.
+		String[] newArray = new String[this.array.length + 1];
+		newArray[0] = element;
+		for (int i = 0; i < this.array.length; i++) {
+			newArray[i + 1] = this.array[i];
+		}
+		this.array = newArray;
 	}
 
 	/**
@@ -39,10 +37,16 @@ public class MyList {
 	 * using the appropriate structure.
 	 */
 	public void removeFirst() {
-		// If the head isn't null, then adjust head to point at its next.
-		if (this.head != null) {
-			this.head = this.head.next;
+		// Very bad implementation, make a new array with one less element than
+		// copy over the elements from the current array starting at index 1.
+		// Then adjust this.array to point at the new array.
+		if (this.array.length > 0) {
+			String[] newArray = new String[this.array.length - 1];
+			for (int i = 1; i < this.array.length; i++) {
+				newArray[i - 1] = this.array[i];
+			}
+			this.array = newArray;
 		}
-	}
 
+	}
 }
